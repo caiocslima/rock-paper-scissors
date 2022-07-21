0efd45a84c8998783613043545a1774643cc2042
Caio <caiolimajf1@gmail.com>
2022-07-21 12:08:13 -0300

    Debugged function order and added choices to log

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+let playerSelection = getPlayerChoice();
+
+let computerSelection = getComputerChoice();
+
+playRound(playerSelection, computerSelection);
+
 function getComputerChoice () {
     const options = ["Rock", "Paper","Scissors"];
     let index = Math.floor(Math.random() * 3);
-    console.log(options[index]);
+   console.log("Computer choice: " + options[index]);
     return options[index];
 }
 function capitalize (text) {
@@ -10,36 +16,29 @@ function capitalize (text) {
 function getPlayerChoice () {
     let option = prompt("Please enter your choice: Rock, Paper or Scissors");
     option = capitalize(option);
-    console.log(option);
+    console.log("Your choice: " + option);
     return option;
 }
 function playRound (player,computer) {
-    if (player === "Rock" && computer === "Paper") {
+    if (player == "Rock" && computer == "Paper") {
         console.log("You lose! Paper beats Rock.");
     }
-    else if (player === "Paper" && computer === "Rock") {
+    else if (player == "Paper" && computer == "Rock") {
         console.log("You win! Paper beats Rock.");
     }
-    else if (player === "Rock" && computer === "Scissors") {
+    else if (player == "Rock" && computer == "Scissors") {
         console.log("You win! Rock beats Scissors.");
     }
-    else if (player === "Scissors" && computer === "Rock") {
+    else if (player == "Scissors" && computer == "Rock") {
         console.log("You lose! Rock beats Scissors.");
     }
-    else if (player === "Paper" && computer === "Scissors") {
+    else if (player == "Paper" && computer == "Scissors") {
         console.log("You lose! Scissors beats Paper.");
     }
-    else if (player === "Scissors" && computer === "Paper") {
+    else if (player == "Scissors" && computer == "Paper") {
         console.log("You win! Scissors beats Paper.");
     }
     else {
         console.log("It's a draw!");
     }
-
-let playerSelection = getPlayerChoice();
-
-let computerSelection = getComputerChoice();
-
-playRound(playerSelection,computerSelection);
-
 }
